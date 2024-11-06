8ba469a2e91011009af1ebc106d1c61e21620096
arthur <arthurmorof@hotmail.com>
2024-11-06 10:06:46 -0300

    refactor: refatora cookie service

--- backend/src/services/cookie-service.js
+++ backend/src/services/cookie-service.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 const cookieService = {
-    createCookie: (username) => {
+    createCookie(username) {
         const token = jwt.sign({ username: username }, 'PGDB', { expiresIn: '24h' });
         return token;
     },
 
-    validateCookie: (token) => {
+    validateCookie(token) {
         try {
             const decoded = jwt.verify(token, 'PGDB');
             return decoded.username;
@@ -15,7 +15,7 @@ const cookieService = {
         }
     },
 
-    validateTokenRoute: async (req, res) => {
+    async validateTokenRoute (req, res) {
         if (!req.cookies || !req.cookies.token) {
             res.status(401).send({ success: false, message: 'no cookie' });
             return;
@@ -29,7 +29,7 @@ const cookieService = {
             res.status(401).send({ success: false, message: 'invalid cookie' })
         }
     },
-    logout : async (req, res) => {
+    async logout(req, res) {
         res.clearCookie('token');
         res.status(200).send({ success: true, message: 'cookie cleared' });
     }
